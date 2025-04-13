766bc0943ac720271cc9bfd5be0dc22612c40663
amina01hu <amina01hu@gmail.com>
2025-04-13 16:45:44 -0400

    adding keyboard input

--- wordle-clone.py
+++ wordle-clone.py
@@ -5,11 +5,117 @@ list = ['rebus', 'siege', 'banal', 'gorge', 'query', 'abbey', 'proxy', 'aloft']
 pygame.init()
 screen = pygame.display.set_mode((633, 900))
 clock = pygame.time.Clock()
-starting_surf = pygame.image.load("assets/starting-squares.png")
-starting_rect = starting_surf.get_rect(center = (316, 300))
-print(starting_rect.x)
+text_font = pygame.font.Font('font/testFont.ttf', 50)
+guess = []
 
-# DRAW STARTING SQUARES 6 rows of 5 letter words
+def drawSquares():
+    rows = 6
+    letters = 5
+    x_pos = 148
+    y_pos = 100
+    for i in range(rows):
+        for j in range(letters):
+            pygame.draw.rect(screen, (120, 124, 127), (x_pos, y_pos, 62, 62), 2)
+            if i == 0 and j < len(guess):  # Only draw letters for the current row
+                letter_surface = text_font.render(guess[j], False, (0, 0, 0))
+                letter_rect = letter_surface.get_rect(center=(x_pos + 31, y_pos + 40))
+                screen.blit(letter_surface, letter_rect)
+            x_pos += 70
+        x_pos = 148
+        y_pos += 70
+
+
+def getKeyPressed():
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_a:
+            addLetter("A")
+        elif event.key == pygame.K_b:
+            addLetter("B")
+        elif event.key == pygame.K_c:
+            addLetter("C")
+        elif event.key == pygame.K_d:
+            addLetter("D")
+        elif event.key == pygame.K_e:
+            addLetter("E")
+        elif event.key == pygame.K_f:
+            addLetter("F")
+        elif event.key == pygame.K_g:
+            addLetter("G")
+        elif event.key == pygame.K_h:
+            addLetter("H")
+        elif event.key == pygame.K_i:
+            addLetter("I")
+        elif event.key == pygame.K_j:
+            addLetter("J")
+        elif event.key == pygame.K_k:
+            addLetter("K")
+        elif event.key == pygame.K_l:
+            addLetter("L")
+        elif event.key == pygame.K_m:
+            addLetter("M")
+        elif event.key == pygame.K_n:
+            addLetter("N")
+        elif event.key == pygame.K_o:
+            addLetter("O")
+        elif event.key == pygame.K_p:
+            addLetter("P")
+        elif event.key == pygame.K_q:
+            addLetter("Q")
+        elif event.key == pygame.K_r:
+            addLetter("R")
+        elif event.key == pygame.K_s:
+            addLetter("S")
+        elif event.key == pygame.K_t:
+            addLetter("T")
+        elif event.key == pygame.K_u:
+            addLetter("U")
+        elif event.key == pygame.K_v:
+            addLetter("V")
+        elif event.key == pygame.K_w:
+            addLetter("W")
+        elif event.key == pygame.K_x:
+            addLetter("X")
+        elif event.key == pygame.K_y:
+            addLetter("Y")
+        elif event.key == pygame.K_z:
+            addLetter("Z")
+        elif event.key == pygame.K_BACKSPACE:
+            removeLetter()
+        elif event.key == pygame.K_RETURN:
+            return checkWord()
+    return True
+
+def addLetter(letter):
+    if len(guess) < 5:
+        guess.append(letter)
+        print(letter)
+        print(guess)
+    else:
+        print("filled")
+    # for i in range(rows):
+    #     for j in range(letters):
+    #         pygame.draw.rect(screen, (120, 124, 127), (x_pos, y_pos, 62, 62), 2)
+    #         x_pos += 70
+    #     x_pos = 148
+    #     y_pos += 70
+    
+def removeLetter():
+    if guess:
+        guess.pop()
+        print(guess)
+    else:
+        print("empty")
+
+def checkWord():
+    if guess and len(guess) == 5:
+        if "".join(guess) == "WEIRD":
+            print("You win")
+            return False
+        else:
+            print("Try again")
+    else:
+        print("word not complete, len: ", len(guess))
+    return True
 
 game_active = True
 score = 0
@@ -26,42 +132,20 @@ while True:
         else:
             if game_active:
                 screen.fill((255, 255, 255))
-                # screen.blit(starting_surf, starting_rect)
-                # first row
-                pygame.draw.rect(screen, (120, 124, 127), (148, 100, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (148, 170, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (148, 240, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (148, 310, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (148, 380, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (148, 450, 62, 62))
-                # second row
-                pygame.draw.rect(screen, (120, 124, 127), (218, 100, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (218, 170, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (218, 240, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (218, 310, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (218, 380, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (218, 450, 62, 62))
-                # third row
-                pygame.draw.rect(screen, (120, 124, 127), (288, 100, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (288, 170, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (288, 240, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (288, 310, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (288, 380, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (288, 450, 62, 62))
-                # fourth row
-                pygame.draw.rect(screen, (120, 124, 127), (358, 100, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (358, 170, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (358, 240, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (358, 310, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (358, 380, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (358, 450, 62, 62))
-                # fifth row
-                pygame.draw.rect(screen, (120, 124, 127), (428, 100, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (428, 170, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (428, 240, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (428, 310, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (428, 380, 62, 62))
-                pygame.draw.rect(screen, (120, 124, 127), (428, 450, 62, 62))
+                # draw starting squares
+                drawSquares()
+                # get keyboard input
+                game_active = getKeyPressed()
+            else:
+                pygame.quit()
+                exit()
+                
+                
+                
+            
+                
+                
+                
                 
                 
     pygame.display.update()
